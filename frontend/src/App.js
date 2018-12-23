afac75ab64d72d8d23edd6f21f19ed3d92af0c2c
Chris Atanasian <catanasian@gmail.com>
2018-12-23 11:48:43 -0800

    Add basic form with submit button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,21 +2,35 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  submitPost() {
+    // fetch('http://localhost:4000/posts/', {
+    //   method: 'GET',
+    //   headers: new Headers({
+    //     'content-type': 'application/json',
+    //   }),
+    // })
+    //   .then(response => response.json())
+    //   .then((responseData) => {
+    //   })
+    //   .catch((e) => {
+    //     console.log(e);
+    //   });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>
+            SecureShare
+          </h1>
+          <form onSubmit={this.submitPost}>
+            <label className="label">
+              Enter your content to be securely shared:
+              <textarea className="input" type="text" name="content" />
+            </label>
+            <input className="submit" type="submit" value="Submit" />
+          </form>
         </header>
       </div>
     );
