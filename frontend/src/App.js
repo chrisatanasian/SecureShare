7575fee4028c1c0ecc0f003f834895f97b12e0a9
Chris Atanasian <catanasian@gmail.com>
2018-12-23 21:23:51 -0800

    Make POST request when clicking submit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,19 +2,30 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  submitPost() {
-    // fetch('http://localhost:4000/posts/', {
-    //   method: 'GET',
-    //   headers: new Headers({
-    //     'content-type': 'application/json',
-    //   }),
-    // })
-    //   .then(response => response.json())
-    //   .then((responseData) => {
-    //   })
-    //   .catch((e) => {
-    //     console.log(e);
-    //   });
+  constructor(props) {
+    super(props);
+    this.state = { content: '' };
+  }
+
+  submitPost = () => {
+    fetch('http://localhost:4000/posts/', {
+      method: 'POST',
+      headers: new Headers({
+        'content-type': 'application/json',
+      }),
+      body: JSON.stringify({ content: this.state.content }),
+    })
+      .then(response => response.json())
+      .then((responseData) => {
+        console.log(responseData);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
+
+  updateContent = (event) => {
+    this.setState({ content: event.target.value });
   }
 
   render() {
@@ -24,13 +35,13 @@ class App extends Component {
           <h1>
             SecureShare
           </h1>
-          <form onSubmit={this.submitPost}>
-            <label className="label">
-              Enter your content to be securely shared:
-              <textarea className="input" type="text" name="content" />
-            </label>
-            <input className="submit" type="submit" value="Submit" />
-          </form>
+          <label className="label">
+            Enter your content to be securely shared:
+          </label>
+          <textarea className="input" type="text" name="content" value={this.state.content} onChange={this.updateContent} />
+          <button type="button" className="submit" onClick={this.submitPost}>
+            Submit
+          </button>
         </header>
       </div>
     );
